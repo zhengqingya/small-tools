be3839b877a119b4d4c2765695d81591ccd07e58
zhengqingya <960869719@qq.com>
2021-07-16 01:02:09 +0800

    add small-tools-api-run-local.sh

--- small-tools-api/small-tools-api-run-local.sh
+++ small-tools-api/small-tools-api-run-local.sh
@@ -7,6 +7,7 @@ APP_PROFILE=dev
 APP_NACOS_SERVER_ADDR='127.0.0.1:8848'
 JAVA_OPTS="-XX:+UseG1GC -Xms64m -Xmx64m -Xmn16m -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=100m -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=8 -Dfile.encoding="UTF-8" -Ddefault.client.encoding="UTF-8" -Dfile.encoding="UTF-8" -Duser.language="Zh" -Duser.region="CN" -Dspring.profiles.active=${APP_PROFILE} -Dspring.cloud.nacos.discovery.server-addr=${APP_NACOS_SERVER_ADDR}"
 
+
 # 停止
 function stop(){
   echo "<-------------------------------------->"
@@ -40,7 +41,7 @@ function buildCommon(){
 
 # 打包
 function build(){
-  buildCommon
+#  buildCommon
 
   # 让mac的maven全局配置生效
   source ~/.bash_profile
@@ -48,13 +49,18 @@ function build(){
   echo "[${SERVICE_NAME}] ... build ..."
 
   # 判断`SERVICE_NAME`是否包含`gateway` 或者 等于`gateway-web`
+  is_gateway = 0
   if [[ ${SERVICE_NAME} = *'gateway'* ]] || [[ ${SERVICE_NAME} = 'gateway-web' ]];then
     echo "网关特殊处理"
+    is_gateway=1
     cd ${SERVICE_HOME}/gateway/${SERVICE_NAME}
   else
     echo "普通服务处理"
+    is_gateway=0
     cd ${SERVICE_HOME}/service/${SERVICE_NAME}
   fi
+
+  echo "当前服务是否为网关：${is_gateway}"
 
   echo "当前路径:`pwd`"
 
@@ -62,8 +68,14 @@ function build(){
   # 创建target目录存放jar包
   rm -rf ${SERVICE_HOME}/docker/target
   mkdir ${SERVICE_HOME}/docker/target
+
   # 拷贝jar
-  cp ${SERVICE_HOME}/service/${SERVICE_NAME}/target/*.jar ${SERVICE_HOME}/docker/target/
+  if [[ ${is_gateway} = 1 ]];then
+     cp ${SERVICE_HOME}/${SERVICE_NAME}/target/*.jar ${SERVICE_HOME}/docker/target/
+  else
+     cp ${SERVICE_HOME}/service/${SERVICE_NAME}/target/*.jar ${SERVICE_HOME}/docker/target/
+  fi
+
   echo "[${SERVICE_NAME}] build success"
 }
 
@@ -74,8 +86,8 @@ function start(){
   echo "[${SERVICE_NAME}] ... start ..."
   cd ${SERVICE_HOME}/docker/target
   echo "当前路径:`pwd`"
-  echo "启动命令: nohup java -jar ${JAVA_OPTS} ${SERVICE_NAME}.jar >> ../logs/${SERVICE_NAME}.log  2>&1 &"
-  nohup java -jar ${JAVA_OPTS} ${SERVICE_NAME}.jar >> ../logs/${SERVICE_NAME}.log  2>&1 &
+  echo "启动命令: nohup java -jar ${JAVA_OPTS} ${SERVICE_NAME}.jar >> ${SERVICE_HOME}/logs/${SERVICE_NAME}.log  2>&1 &"
+  nohup java -jar ${JAVA_OPTS} ${SERVICE_NAME}.jar >> ${SERVICE_HOME}/logs/${SERVICE_NAME}.log  2>&1 &
   if [ $? -eq 0 ]; then
     echo "[${SERVICE_NAME}] start success"
   else
