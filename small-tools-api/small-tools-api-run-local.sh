0d0210da90553c8b953c1f6ae9a9291a5696ffa7
zhengqingya <960869719@qq.com>
2021-07-26 22:59:58 +0800

    update small-tools-api-run-local.sh

--- small-tools-api/small-tools-api-run-local.sh
+++ small-tools-api/small-tools-api-run-local.sh
@@ -16,34 +16,48 @@ set -eu
 
 # 检查参数个数
 if [ "${#}" -lt 2 ]; then
-	echo "\033[41;37m 脚本使用示例: sh small-tools-api-run-local.sh start|stop|restart app \033[0m"
+	echo "\033[41;37m 脚本使用示例: sh small-tools-api-run-local.sh build|start|stop|restart app \033[0m"
 	exit
 fi
 
 # 获取脚本第一个参数
 APP_OPT=${1}
 # 端口
-APP_PORT=10000
+#APP_PORT=10000
 # 程序根目录
 APP_HOME='/Users/zhengqingya/IT_zhengqing/code/small-tools/small-tools-api'
 # 名称
-APP_NAME=${2}
-# jar名 | war名
-APP_JAR=${APP_NAME}.jar
+APP=${2}
 # 程序jar目录
 APP_JAR_HOME="${APP_HOME}/target"
-# 日志名
-APP_LOG_NAME=${APP_NAME}
 # 日志根目录
 APP_LOG_HOME="${APP_HOME}/logs"
 # 程序运行参数
 APP_PROFILE=dev
 APP_NACOS_SERVER_ADDR='127.0.0.1:8848'
 JAVA_OPTS="-XX:+UseG1GC -Xms64m -Xmx64m -Xmn16m -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=100m -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=8 -Dfile.encoding="UTF-8" -Ddefault.client.encoding="UTF-8" -Dfile.encoding="UTF-8" -Duser.language="Zh" -Duser.region="CN" -Dspring.profiles.active=${APP_PROFILE} -Dspring.cloud.nacos.discovery.server-addr=${APP_NACOS_SERVER_ADDR}"
+# 服务名数组
+APP_NAME_ARRAY=""
 
 
-echo "本次操作服务名：[${APP_NAME}]"
+echo "本次操作服务：[${APP}]"
 echo "本次操作选择：[${APP_OPT}]"
+
+
+# 将逗号分隔的字符串服务名分隔成数组
+function divide_app_name() {
+  echo "<-------------------------------------->"
+  echo "[${APP}] ... divide ..."
+  echo "分割后操作服务名:"
+  # 以逗号分隔
+  APP_NAME_ARRAY=(${APP//\,/ })
+  for((i=0;i<${#APP_NAME_ARRAY[@]};i++)); do
+    echo "                  [$i=>${APP_NAME_ARRAY[i]}]"
+  done
+}
+
+divide_app_name
+
 
 # 判断jar目录是否存在
 if [ -d "${APP_JAR_HOME}" ];
@@ -66,98 +80,125 @@ else
 fi
 
 
-# 停止
-function stop(){
-  echo "<-------------------------------------->"
-  echo "[${APP_NAME}] ... stop ..."
-  # 查看该jar进程
-  pid=`ps -ef | grep ${APP_JAR} | grep -v 'grep' | awk '{print $2}'`
-  echo "[${APP_NAME}] pid="${pid}
-  # 存在则kill,不存在打印一下吧
-  if [ "${pid}" ]; then
-    kill -9 ${pid}
-      # 检查kill是否成功
-      if [ "$?" -eq 0 ]; then
-          echo "[${APP_NAME}] stop success"
-      else
-          echo "[${APP_NAME}] stop fail"
-      fi
-  else
-    echo "[${APP_NAME}] 进程不存在"
-  fi
-}
-
 
 # 打包 - common
-function buildCommon(){
+function build_common(){
   echo "<-------------------------------------->"
-  echo "[${APP_NAME}] ... build common ..."
+  echo "[${APP}] ... build common ..."
   cd ${APP_HOME}/common
   echo "当前路径:`pwd`"
   mvn clean install -Dmaven.test.skip=true
-  echo "[${APP_NAME}] build common success"
+  echo "[${APP}] build common success"
 }
 
 # 打包
 function build(){
-  buildCommon
-
   echo "<-------------------------------------->"
-  echo "[${APP_NAME}] ... build ..."
+  echo "[${APP}] ... build ..."
+  for((i=0;i<${#APP_NAME_ARRAY[@]};i++)); do
+    app_name=${APP_NAME_ARRAY[i]}
+    echo "打包服务： [${app_name}]"
 
-  # 判断`APP_NAME`是否包含`gateway` 或者 等于`gateway-web`
-  is_gateway=0
-  if [[ ${APP_NAME} = *'gateway'* ]] || [[ ${APP_NAME} = 'gateway-web' ]];then
-    echo "网关特殊处理"
-    is_gateway=1
-    cd ${APP_HOME}/gateway/${APP_NAME}
-  else
-    echo "普通服务处理"
+    # 判断`app_name`是否包含`gateway` 或者 等于`gateway-web`
     is_gateway=0
-    cd ${APP_HOME}/service/${APP_NAME}
-  fi
+    if [[ ${app_name} = *'gateway'* ]] || [[ ${app_name} = 'gateway-web' ]];then
+      echo "网关特殊处理"
+      is_gateway=1
+      cd ${APP_HOME}/gateway/${app_name}
+    else
+      echo "普通服务处理"
+      is_gateway=0
+      cd ${APP_HOME}/service/${app_name}
+    fi
 
-  echo "当前服务是否为网关：${is_gateway}"
-  echo "当前路径:`pwd`"
+    echo "当前服务是否为网关：${is_gateway}"
+    echo "当前路径:`pwd`"
 
-  # 打包
-  mvn clean install -Dmaven.test.skip=true
+    # 打包
+    mvn clean install -Dmaven.test.skip=true
 
-  # 拷贝jar
-  if [[ ${is_gateway} = 1 ]];then
-     cp ${APP_HOME}/${APP_NAME}/target/*.jar ${APP_JAR_HOME}/
-  else
-     cp ${APP_HOME}/service/${APP_NAME}/target/*.jar ${APP_JAR_HOME}/
-  fi
+    echo "[${app_name}] build success"
+  done
+}
 
-  echo "[${APP_NAME}] build success"
+
+# 拷贝jar
+function copy_jar() {
+  echo "<-------------------------------------->"
+  echo "[${APP}] ... copy_jar ..."
+  for((i=0;i<${#APP_NAME_ARRAY[@]};i++)); do
+    app_name=${APP_NAME_ARRAY[i]}
+    echo "拷贝服务： [${app_name}]"
+    if [[ ${app_name} = *'gateway'* ]] || [[ ${app_name} = 'gateway-web' ]];then
+      cp ${APP_HOME}/${app_name}/target/*.jar ${APP_JAR_HOME}/
+    else
+      cp ${APP_HOME}/service/${app_name}/target/*.jar ${APP_JAR_HOME}/
+    fi
+  done
+}
+
+
+# 停止
+function stop(){
+  echo "<-------------------------------------->"
+  echo "[${APP}] ... stop ..."
+  for((i=0;i<${#APP_NAME_ARRAY[@]};i++)); do
+    app_name=${APP_NAME_ARRAY[i]}
+    echo "停止服务： [${app_name}]"
+    # 查看该jar进程
+    pid=`ps -ef | grep ${app_name}.jar | grep -v 'grep' | awk '{print $2}'`
+    echo "[${app_name}] pid="${pid}
+    # 存在则kill,不存在打印一下吧
+    if [ "${pid}" ]; then
+      kill -9 ${pid}
+        # 检查kill是否成功
+        if [ "$?" -eq 0 ]; then
+            echo "[${app_name}] stop success"
+        else
+            echo "[${app_name}] stop fail"
+        fi
+    else
+      echo "[${app_name}] 进程不存在"
+    fi
+  done
 }
 
 
 # 运行
 function start(){
-  build
   echo "<-------------------------------------->"
-  echo "[${APP_NAME}] ... start ..."
-  cd ${APP_JAR_HOME}
-  echo "当前路径:`pwd`"
-  # 赋予可读可写可执行权限
-  chmod 777 ${APP_JAR}
-  echo "启动命令: nohup java -jar ${APP_JAR} ${JAVA_OPTS} >> ${APP_LOG_HOME}/${APP_LOG_NAME}.log 2>&1 &"
-  nohup java -jar ${APP_JAR} ${JAVA_OPTS} >> ${APP_LOG_HOME}/${APP_LOG_NAME}.log 2>&1 &
-  if [ "$?" -eq 0 ]; then
-    echo "[${APP_NAME}] start success"
-  else
-    echo "[${APP_NAME}] start fail"
-  fi
+  echo "[${APP}] ... start ..."
+  for((i=0;i<${#APP_NAME_ARRAY[@]};i++)); do
+    app_name=${APP_NAME_ARRAY[i]}
+    app_jar="${app_name}.jar"
+    echo "运行服务： [${app_name}]"
+    cd ${APP_JAR_HOME}
+    echo "当前路径:`pwd`"
+    # 赋予可读可写可执行权限
+    chmod 777 ${app_jar}
+    echo "启动命令: nohup java -jar ${app_jar} ${JAVA_OPTS} >> ${APP_LOG_HOME}/${app_name}.log 2>&1 &"
+    nohup java -jar ${app_jar} ${JAVA_OPTS} >> ${APP_LOG_HOME}/${app_name}.log 2>&1 &
+    if [ "$?" -eq 0 ]; then
+      echo "[${app_name}] start success"
+    else
+      echo "[${app_name}] start fail"
+    fi
+  done
 }
 
 
 # 重启
 function restart(){
   echo "<-------------------------------------->"
-  echo "[${APP_NAME}] ... restart ..."
-	stop
+  echo "[${APP}] ... restart ..."
+  # 打包
+  build_common
+  build
+  # 拷贝jar
+  copy_jar
+  # 停止服务
+  stop
+  # 启动服务
 	start
 }
 
@@ -165,15 +206,22 @@ function restart(){
 # 多分支条件判断执行参数
 case "${APP_OPT}" in
  "build")
+    # 打包
+    build_common
 		build
+		# 拷贝jar
+    copy_jar
 		;;
 	"stop")
+	  # 停止服务
 		stop
 		;;
 	"start")
+    # 运行服务
 		start
 		;;
 	"restart")
+	  # 重启服务
 		restart
 		;;
 	*)
