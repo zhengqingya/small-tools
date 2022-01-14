46f61c495493d3a47f7d93b1325278122e9a7f03
zhengqingya <960869719@qq.com>
2022-01-14 23:04:31 +0800

    update RedissonConfig.java

---
+++ small-tools-api/common/src/main/java/com/zhengqing/common/config/redis/RedissonConfig.java
@@ -0,0 +1,38 @@
+package com.zhengqing.common.config.redis;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p> Redisson配置类 </p>
+ *
+ * @author zhengqingya
+ * @description
+ * @date 2022/1/14 11:03 下午
+ */
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private String port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Bean
+    public RedissonClient getRedisson() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + host + ":" + port).setPassword(password);
+        //添加主从配置
+//        config.useMasterSlaveServers().setMasterAddress("").setPassword("").addSlaveAddress(new String[]{"",""});
+        return Redisson.create(config);
+    }
+
+}
