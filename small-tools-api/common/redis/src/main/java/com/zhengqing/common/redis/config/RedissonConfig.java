ccdae92bb841d07d230e2f97cbea5a63cfc83428
zhengqingya <960869719@qq.com>
2022-06-20 10:12:50 +0800

    update RedissonConfig

--- small-tools-api/common/redis/src/main/java/com/zhengqing/common/redis/config/RedissonConfig.java
+++ small-tools-api/common/redis/src/main/java/com/zhengqing/common/redis/config/RedissonConfig.java
@@ -26,10 +26,25 @@ public class RedissonConfig {
     @Value("${spring.redis.password}")
     private String password;
 
+    @Value("${spring.redis.database}")
+    private Integer database;
+
+    @Value("${spring.redis.jedis.pool.min-idle}")
+    private Integer connectionMinimumIdleSize;
+
+    @Value("${spring.redis.timeout}")
+    private Integer timeout;
+
     @Bean
     public RedissonClient getRedisson() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://" + this.host + ":" + this.port).setPassword(this.password);
+        config
+                .useSingleServer()
+                .setAddress("redis://" + this.host + ":" + this.port)
+                .setPassword(this.password)
+                .setDatabase(this.database)
+                .setConnectionMinimumIdleSize(this.connectionMinimumIdleSize)
+                .setTimeout(this.timeout);
         return Redisson.create(config);
     }
 
