5414dd40c7ff545b0b044126a701ce6dd14f125b
zhengqingya <960869719@qq.com>
2022-06-24 11:57:47 +0800

    update druid

--- small-tools-api/common/redis/src/main/java/com/zhengqing/common/redis/config/RedisListenerConfig.java
+++ small-tools-api/common/redis/src/main/java/com/zhengqing/common/redis/config/RedisListenerConfig.java
@@ -1,28 +1,28 @@
-//package com.zhengqing.common.redis.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-//
-///**
-// * <p>
-// * redis监听事件配置
-// * </p>
-// *
-// * @author zhengqingya
-// * @description
-// * @date 2020/11/13 13:56
-// */
-//@Configuration
-//public class RedisListenerConfig {
-//    @Bean
-//    @Primary
-//    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
-//        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-//        container.setConnectionFactory(connectionFactory);
-//        return container;
-//    }
-//
-//}
+package com.zhengqing.common.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+/**
+ * <p>
+ * redis监听事件配置
+ * </p>
+ *
+ * @author zhengqingya
+ * @description
+ * @date 2020/11/13 13:56
+ */
+@Configuration
+public class RedisListenerConfig {
+    @Bean
+    @Primary
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+
+}
