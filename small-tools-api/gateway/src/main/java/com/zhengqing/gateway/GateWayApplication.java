73e404daf461624f3215918ac7951720ed68acf6
zhengqingya <960869719@qq.com>
2022-06-24 18:40:36 +0800

    update note

--- small-tools-api/gateway/src/main/java/com/zhengqing/gateway/GateWayApplication.java
+++ small-tools-api/gateway/src/main/java/com/zhengqing/gateway/GateWayApplication.java
@@ -1,25 +1,24 @@
 package com.zhengqing.gateway;
 
 import com.zhengqing.common.base.constant.ServiceConstant;
-import com.zhengqing.common.web.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
-// 默认只会扫当前模块包，需要额外加上其它模块的
-//@ComponentScan(basePackages = {
-//        "com.zhengqing.gateway",
-//        "com.zhengqing.common",
-//})
 @ComponentScan(
-        basePackages = {ServiceConstant.SERVICE_BASE_PACKAGE},
+        basePackages = {
+                // 默认只会扫当前模块包，需要额外加上其它模块的
+//                "com.zhengqing.gateway",
+//                "com.zhengqing.common.redis",
+                ServiceConstant.SERVICE_BASE_PACKAGE
+        }
         // 排除指定配置类
-        excludeFilters = @ComponentScan.Filter(
-                type = FilterType.ASSIGNABLE_TYPE,
-                value = {WebConfig.class}
-        )
+//        ,
+//        excludeFilters = @ComponentScan.Filter(
+//                type = FilterType.ASSIGNABLE_TYPE,
+//                value = {WebConfig.class}
+//        )
 )
 @EnableDiscoveryClient // 开启服务注册发现功能
 @SpringBootApplication
