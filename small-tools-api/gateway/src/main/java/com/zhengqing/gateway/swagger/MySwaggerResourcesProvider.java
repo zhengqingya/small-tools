78fe0e72f0261fdb7c171ba15004d98d61b84ab6
zhengqingya <960869719@qq.com>
2022-06-27 10:40:21 +0800

    update 通过nacos注册中心服务实例动态聚合各个服务的swagger接口

--- small-tools-api/gateway/src/main/java/com/zhengqing/gateway/swagger/MySwaggerResourcesProvider.java
+++ small-tools-api/gateway/src/main/java/com/zhengqing/gateway/swagger/MySwaggerResourcesProvider.java
@@ -1,7 +1,7 @@
 package com.zhengqing.gateway.swagger;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.SwaggerResource;
@@ -9,13 +9,11 @@ import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * <p>
- * 聚合各个服务的swagger接口
+ * 动态聚合各个服务的swagger接口
  * </p>
  *
  * @author zhengqingya
@@ -33,43 +31,36 @@ public class MySwaggerResourcesProvider implements SwaggerResourcesProvider {
     private static final String SWAGGER_3_URL = "/v3/api-docs?group=";
 
     /**
-     * 网关路由
+     * nacos注册中心
      */
     @Resource
-    private RouteLocator routeLocator;
+    private DiscoveryClient discoveryClient;
 
     /**
      * 网关应用名称
      */
     @Value("${spring.application.name}")
-    private String self;
+    private String gatewayServiceName;
 
     @Override
     public List<SwaggerResource> get() {
-        List<SwaggerResource> resources = new ArrayList<>();
-        List<String> routeHosts = new ArrayList<>();
-        // 获取所有可用的host：serviceId
-        this.routeLocator.getRoutes().filter(route -> route.getUri().getHost() != null)
-                .filter(route -> !this.self.equals(route.getUri().getHost()))
-                .subscribe(route -> routeHosts.add(route.getUri().getHost()));
-
-        // 记录已经添加过的server，存在同一个应用注册了多个服务在nacos上
-        Set<String> dealed = new HashSet<>();
-        routeHosts.forEach(instance -> {
+        // swagger下拉列表服务资源
+        List<SwaggerResource> resourceList = new ArrayList<>();
+        // 动态获取nacos注册中心上的服务实例名
+        List<String> nacosServiceNameList = this.discoveryClient.getServices();
+        for (String instance : nacosServiceNameList) {
             String instanceName = instance.toLowerCase();
+            if (this.gatewayServiceName.equals(instanceName)) {
+                continue;
+            }
             // 拼接swagger访问url，样式为`/demo/v3/api-info?group=demo`，当网关调用这个接口时，会自动通过负载均衡寻找对应的主机
             String url = "/" + instanceName + SWAGGER_3_URL + instanceName;
-            // 无group时 使用默认
-            if (!dealed.contains(url)) {
-                dealed.add(url);
-                SwaggerResource swaggerResource = new SwaggerResource();
-                swaggerResource.setUrl(url);
-                swaggerResource.setName(instance);
-                // 服务下拉列表读取映射
-                resources.add(swaggerResource);
-            }
-        });
-        return resources;
+            SwaggerResource swaggerResource = new SwaggerResource();
+            swaggerResource.setUrl(url);
+            swaggerResource.setName(instance);
+            resourceList.add(swaggerResource);
+        }
+        return resourceList;
     }
 
 }
