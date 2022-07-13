2818d7ab3e9999417b3ef2d929c58bac561309a5
zhengqingya <960869719@qq.com>
2022-07-13 11:41:47 +0800

    update note

--- small-tools-web/src/utils/request.js
+++ small-tools-web/src/utils/request.js
@@ -1,36 +1,36 @@
-import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
-import store from '@/store'
-import { getToken } from '@/utils/auth'
+import axios from "axios";
+import { MessageBox, Message } from "element-ui";
+import store from "@/store";
+import { getToken } from "@/utils/auth";
 
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 50000, // request timeout
-})
+  timeout: 50000 // request timeout
+});
 
 // request interceptor
 service.interceptors.request.use(
-  (config) => {
+  config => {
     // do something before request is sent
 
     if (store.getters.token) {
       // let each request carry token
       // ['Authorization'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['Authorization'] = getToken()
+      config.headers["Authorization"] = getToken();
     }
     // 租户ID
-    config.headers['TENANT_ID'] = 1
-    return config
+    config.headers["TENANT_ID"] = 1;
+    return config;
   },
-  (error) => {
+  error => {
     // do something with request error
-    console.log(error) // for debug
-    return Promise.reject(error)
+    console.log(error); // for debug
+    return Promise.reject(error);
   }
-)
+);
 
 // response interceptor
 service.interceptors.response.use(
@@ -44,34 +44,34 @@ service.interceptors.response.use(
    * Here is just an example
    * You can also judge the status by HTTP Status Code
    */
-  (response) => {
-    const res = response.data
+  response => {
+    const res = response.data;
 
     if (res.code !== 200) {
       Message({
-        message: res.msg || '接口请求报错',
-        type: 'error',
-        duration: 5 * 1000,
-      })
+        message: res.msg || "接口请求报错",
+        type: "error",
+        duration: 5 * 1000
+      });
       if (res.code === -1) {
         // token过期
         // to re-login
-        MessageBox.confirm('您的登录账号已失效，请重新登录', {
-          confirmButtonText: '再次登录',
-          cancelButtonText: '取消',
-          type: 'warning',
+        MessageBox.confirm("您的登录账号已失效，请重新登录", {
+          confirmButtonText: "再次登录",
+          cancelButtonText: "取消",
+          type: "warning"
         }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
-        })
+          store.dispatch("user/resetToken").then(() => {
+            location.reload();
+          });
+        });
       }
-      return Promise.reject(new Error(res.msg || 'Error'))
+      return Promise.reject(new Error(res.msg || "Error"));
     } else {
-      return res
+      return res;
     }
   },
-  (error) => {
+  error => {
     // 接口反爬虫处理 509状态码
     // if (error.response.status === 509) {
     //   const html = error.response.data
@@ -83,26 +83,26 @@ service.interceptors.response.use(
     if (error.response.status === 401) {
       // token过期
       // to re-login
-      MessageBox.confirm('您的登录账号已失效，请重新登录', {
-        confirmButtonText: '再次登录',
-        cancelButtonText: '取消',
-        type: 'warning',
+      MessageBox.confirm("您的登录账号已失效，请重新登录", {
+        confirmButtonText: "再次登录",
+        cancelButtonText: "取消",
+        type: "warning"
       }).then(() => {
-        store.dispatch('user/resetToken').then(() => {
-          location.reload()
-        })
-      })
+        store.dispatch("user/resetToken").then(() => {
+          location.reload();
+        });
+      });
     } else {
-      console.log('错误：' + error) // for debug
+      console.log("错误：" + error); // for debug
       Message({
-        message: '网络异常，请稍后再试!',
-        type: 'error',
-        duration: 5 * 1000,
-      })
+        message: "网络异常，请稍后再试!",
+        type: "error",
+        duration: 5 * 1000
+      });
       // this.submitFail(res.msg)
-      return Promise.reject(error)
+      return Promise.reject(error);
     }
   }
-)
+);
 
-export default service
+export default service;
